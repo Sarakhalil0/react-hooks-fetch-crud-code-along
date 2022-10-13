4dd2b62418e959fdf67be2b4ccd3572e40767fea
sara khalil <sarakhalil3377@gmail.com>
2022-10-13 18:11:02 +0300

    DONE

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  //second
+  function handleSubmit(e) {
+    e.preventDefault()
+    const itemsData = {
+      name: name,
+      category: category,
+      isInCart: false
+    }
+
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemsData)
+    })
+
+      .then(res => res.json())
+      .then(itemsData => onAddItem(itemsData))
+
+  }
+  //third we call onAddItem callbackfunc. as a props an gives it an argument itemData wich is a new item that
+  //user submited
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,40 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  //first
+  
+    // Update state by passing the array of items to setItems  
+    useEffect(() => {
+      fetch("http://localhost:4000/items")
+        .then((r) => r.json())
+        .then((items) => setItems(items));
+    }, []);
+
+
+  //third / parameter   (itemData) dachta nawy
+  function handleAddItem(newItem) {
+    setItems([...items, newItem])
+  }
+
+  // forth /after patch/ add this callback function
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem
+      } else {
+        return item
+      }
+    })
+    setItems(updatedItems)
+  }
+
+  //five wtumana aw item 'ana benawa ka id = nya ba id aw itemay ka dagirawa wata delete button
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -16,21 +50,23 @@ function ShoppingList() {
 
     return item.category === selectedCategory;
   });
+  
 
+  //third adding onAddItem as props to ItemForm
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onDeleteItem={handleDeleteItem} onUpdateItem={handleUpdateItem} />
         ))}
       </ul>
     </div>
   );
 }
-
+//forth adding onUpdateItem={handleUpdateItem} as props
 export default ShoppingList;
